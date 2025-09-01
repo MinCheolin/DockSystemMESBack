d668eddd2e4f6be46e57d3bc04147d796f6664cd
김민철 <rlaalscjf739789@gamil.com>
2025-09-01 09:31:21 +0900

    첫커밋

---
+++ src/main/java/com/example/docksystem_mes/DocksystemMesApplication.java
@@ -0,0 +1,13 @@
+package com.example.docksystem_mes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocksystemMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocksystemMesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docksystem_mes/DocksystemMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docksystem_mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocksystemMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
